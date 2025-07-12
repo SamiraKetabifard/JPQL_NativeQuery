1a2bc015056521d849315b3f51dbe74cd89ec9f7
SamiraKetabifard <samiraketabi.fard@gmail.com>
2025-07-12 18:14:26 +0330

    edit

--- src/main/java/com/example/jpql_nativequery/controller/UserController.java
+++ src/main/java/com/example/jpql_nativequery/controller/UserController.java
@@ -20,42 +20,34 @@ public class UserController {
     public List<UserDto> getActiveUsersJPA() {
         return userService.getActiveUsersJPA();
     }
-
     @GetMapping("/jpql/active")
     public List<UserDto> getActiveUsersJPQL() {
         return userService.getActiveUsersJPQL();
     }
-
     @GetMapping("/native/active")
     public List<UserDto> getActiveUsersNative() {
         return userService.getActiveUsersNative();
     }
-
     @GetMapping("/jpa/findByUsernameAndActiveTrue")
     public List<UserDto> findByUsernameAndActiveTrue(@RequestParam String username) {
         return userService.getUsernameAndActiveTrueJPA(username);
     }
-
     @GetMapping("/jpql/findByUsernameAndActiveTrue")
     public List<UserDto> findByUsernameAndActiveTrueJPQL(@RequestParam String username) {
         return userService.getUsernameAndActiveTrueJPQL(username);
     }
-
     @GetMapping("/native/findByUsernameAndActiveTrue")
     public List<UserDto> findByUsernameAndActiveTrueNative(@RequestParam String username) {
         return userService.getUsernameAndActiveTrueNative(username);
     }
-
     @GetMapping("/jpa/countActiveUsers")
     public long countActiveUsersJPA() {
         return userService.getCountActiveUsers();
     }
-
     @GetMapping("/jpql/countActiveUsers")
     public long countActiveUsersJPQL() {
         return userService.getCountActiveUsersJPQL();
     }
-
     @GetMapping("/native/countActiveUsers")
     public long countActiveUsersNative() {
         return userService.getCountActiveUsersNative();
